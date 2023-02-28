b3879ae21869e5c0099ad33054df930e1b0ef52f
aditya <exp.aditya.bhatt.12@gmail.com>
2023-02-28 12:05:48 +0530

    adding day 2 tasks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,13 +18,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-
-      <Greet >
-        <h2>hey this is another header</h2>
-      </Greet>
-
-      {/* <Greet /> */}
+      </header> */}
+      <Home />
 
     </div>
   );
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Title from '../components/Title'
+
+export default class 
+ extends Component {
+  render() {
+    return <h2>
+        <Title title = {"title1"}/>
+        <Title title = {"title2"}/>
+        <Title title = {"title3"}/>
+    </h2>
+  }
+}
